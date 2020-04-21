2d1c455d2c02bfe056b763627f9591d55fe0424c
David den Otter <dden@computronix.com>
2020-04-20 21:21:56 -0600

    Initial Commit

--- PolygonColisions/main.cpp
+++ PolygonColisions/main.cpp
@@ -1,10 +1,10 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
-class Example : public olc::PixelGameEngine {
+class PolygonCollisions : public olc::PixelGameEngine {
     public:
-        Example() {
-            sAppName = "Example";
+        PolygonCollisions() {
+            sAppName = "Polygon Collisions";
         }
 
     public:
@@ -15,19 +15,13 @@ class Example : public olc::PixelGameEngine {
 
         bool OnUserUpdate(float fElapsedTime) override {
             Clear(olc::BLACK);
-            // called once per frame
-            for (int x = 0; x < ScreenWidth(); x++)
-                for (int y = 0; y < ScreenHeight(); y++)
-                    Draw(x, y, olc::Pixel(rand() % 255, rand() % 255, rand()% 255));
-            
-            FillRect(GetMouseX(), GetMouseY(), 1, 1);
             return true;
         }
 };
 
 
 int main(int argc, char const *argv[]) {
-	Example demo;
+	PolygonCollisions demo;
 	if (demo.Construct(15, 10, 32, 32))
 		demo.Start();
 
